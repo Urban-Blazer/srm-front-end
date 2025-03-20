1ef0b93924b18d13985492d056ebbeeb08491eef
Urban-Blazer <urbanblazer@proton.me>
2025-03-19 17:24:21 -0700

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '../app/components/Navbar';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const GA_MEASUREMENT_ID = "G-03BHKQVRD6"; // Replace with your actual Google Analytics ID
 
 export const metadata: Metadata = {
   title: 'Sui Rewards Me',
@@ -14,7 +17,7 @@ export const metadata: Metadata = {
     title: 'Sui Rewareds Me',
     description:
       'The First Rewards Descentralized Exchange DEX Ever. It is time you got a piece',
-    images: '/banner.jpg',
+    images: '/images/banner.jpg',
     card: 'summary_large_image',
     site: '@SuiRewardsMe',
   },
@@ -22,7 +25,7 @@ export const metadata: Metadata = {
     title: 'Sui Rewards Me',
     description:
       'The First Rewards Descentralized Exchange DEX Ever. It is time you got a piece',
-    images: '/preview.png',
+    images: '/images/banner.jpg',
     url: 'https://SuiRewards.Me',
     type: 'website',
   },
@@ -35,6 +38,27 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        {/* Google Analytics Scripts */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} bg-gray-900 text-white h-screen`}>
         <Navbar />
         <main className="container mx-auto">{children}</main>

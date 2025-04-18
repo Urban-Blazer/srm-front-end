7e075481650685033ce5e308f00ee55e8432ad6a
Urban-Blazer <urbanblazer@proton.me>
2025-04-18 11:32:00 -0700

    Update Config - Live contract

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,7 @@
 export const GETTER_RPC = "https://sui-mainnet.blastapi.io/9869b313-c92e-4bed-9bd6-4cfa31ce05fb";
-export const PACKAGE_ID = "0xad77a02b9900105de234e7eeda8e2e28d730330a28b72295ee8c87969645782e";
+export const PACKAGE_ID = "0xa3ae89c38ed81e443d3954ace58ca682ee3705b735d088e7a552b31c59f72f8c";
 export const DEX_MODULE_NAME = "srmV1";
 export const QUOTE_MODULE_NAME = "quote";
-export const FACTORY_ID = "0x7275d6367fdd591db3cebcc0228cdbaa294f2849f24654a7f5853d6dc75d1122";
-export const CONFIG_ID = "0x85aea0feb28004d3adf44a25e182239e0441293d99684e8360554e46fe8b76b8";
-export const LOCK_ID = "0x6cc8cd4997881c4f7c84ba0d1ffa4e238742f65f5f298787eac2125f2f539f7c";
+export const FACTORY_ID = "0x1ced8bbbb30b1392c3e68f0b80ce1dd6d78e81a58b43fa3d2cbab03194ca749c";
+export const CONFIG_ID = "0xfddc5998480267f0ca4e8c1ce3e77ed7d168901b2bf3a4018fcff81669c77f09";
+export const LOCK_ID = "0x1801ab913ca5777dc3f0ac6f5719868fa666436b1ddd35dab55f0e7d936160eb";
